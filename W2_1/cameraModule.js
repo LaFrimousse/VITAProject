a0c3b485c0543419d7d91662c98343a10ce4eb67
LaFrimousse <kaeser.jonathan@gmail.com>
2019-02-26 18:33:28 +0100

    renamed some files and moduled the already existing code

---
+++ W2_1/cameraModule.js
@@ -0,0 +1,94 @@
+//tuto : https://www.html5rocks.com/en/tutorials/getusermedia/intro/
+'use strict'
+
+var cameraModule = (function () {
+
+  //the DOM element in which the video is displayed
+  var videoElement = document.getElementById("videoElement")
+  //a helper variable that keeps track of the camera state
+  var isCameraOpen = false
+
+/*some constraints about the video format that can still evolve, we can pick
+ the one we want */
+  const constraints = {
+    video: true
+  };
+  const vgaConstraints = {
+    video: {width: {exact: 640}, height: {exact: 480}}
+  };
+  const hdConstraints = {
+    video: {width: {min: 1280}, height: {min: 720}}
+  };
+
+  var hasUserMedia = function() {
+    var hasMedia = !!(navigator.mediaDevices &&
+      navigator.mediaDevices.getUserMedia)
+
+      if(hasMedia){
+        return true
+      }else{
+        alert("getUserMedia() is not supported by your browser, you won't be able to use this web page!");
+        return false
+      }
+  }
+
+/* the callback is called with a true value iff the camera was closed before the call of this function
+and will be open after the call of this function*/
+  var openCamera = function(callback){
+
+    if(this.isCameraOpen || !hasUserMedia()){
+      if (typeof(callback) != "undefined"){
+        callback(false)
+      }
+      return
+    }
+    /*With the following three lines of code we can already film ourself */
+
+    navigator.mediaDevices.getUserMedia(constraints).//or vgaConstraints or hd Constraints
+      then((stream) => {
+        videoElement.srcObject = stream
+        this.isCameraOpen = true
+        if (typeof(callback) != "undefined"){
+          callback(true)
+        }
+      }).catch(function (error) {
+         console.error("Error: Promise Rejected", error)
+         if (typeof(callback) != "undefined"){
+           callback(false)
+         }
+    });
+  }
+
+  /* the callback is called with a true value iff the camera was open before the call of this function
+  and will be closed after the call of this function*/
+  var closeCamera =  function(callback){
+
+    if(!this.isCameraOpen){
+      if (typeof(callback) != "undefined"){
+        callback(false)
+      }
+      return
+    }
+
+    let stream = videoElement.srcObject;
+    let tracks = stream.getTracks();
+
+    tracks.forEach(function(track) {
+      track.stop();
+    });
+
+    videoElement.srcObject = null;
+    this.isCameraOpen = false
+    if (typeof(callback) != "undefined"){
+      callback(true)
+    }
+  }
+
+  /*Explicitly reveal public pointers to the private functions
+   that we want to reveal publicly*/
+  return {
+    openCamera: openCamera,
+    closeCamera: closeCamera,
+    isCameraOpen: isCameraOpen
+  }
+})();
