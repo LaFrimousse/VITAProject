15953cf19de4ae8fc403eafa69dea3fc712b9fc9
LaFrimousse <kaeser.jonathan@gmail.com>
2019-05-19 13:47:27 +0200

    resolved some bugs

--- W12/js/main.js
+++ W12/js/main.js
@@ -1,95 +1,95 @@
 (function(window) {
-  'use strict';
+    'use strict';
 
-  var App = window.App;
+    var App = window.App;
 
-  var CameraEvents = App.CameraEvents;
-  var CategoriesStorage = App.CategoriesStorage;
-  var CategoriesLayout = App.CategoriesLayout;
-  var Firebase = App.Firebase;
-  var Helper = App.Helper;
-  var Server = App.Server;
-  var PointsDrawing = App.PointsDrawing;
-  var PifPafBuffer = App.PifPafBuffer;
+    var CameraEvents = App.CameraEvents;
+    var CategoriesStorage = App.CategoriesStorage;
+    var CategoriesLayout = App.CategoriesLayout;
+    var Firebase = App.Firebase;
+    var Helper = App.Helper;
+    var Server = App.Server;
+    var PointsDrawing = App.PointsDrawing;
+    var PifPafBuffer = App.PifPafBuffer;
 
-  /*var CategoriesManager = App.CategoriesManager;
-  var Server = App.Server;
-  var PointsDrawing = App.PointsDrawing;*/
+    /*var CategoriesManager = App.CategoriesManager;
+    var Server = App.Server;
+    var PointsDrawing = App.PointsDrawing;*/
 
-  var Manager = function() {
-    var verbose = true;
-    var drawLivePoints = false;
+    var Manager = function() {
+      var verbose = true;
+      var drawLivePoints = false;
 
-    var clientIdCookieName = "clientIdCookieName";
-    var clientId = null;
+      var clientIdCookieName = "clientIdCookieName";
+      var clientId = null;
 
-    (function() { // init code that fetch the client id cookie
-      var previousClientId = Helper.getCookie(clientIdCookieName);
-      if (previousClientId == null) {
-        if (verbose) {
-          console.log("Main: no previous cookie found for this user identifier");
+      (function() { // init code that fetch the client id cookie
+        var previousClientId = Helper.getCookie(clientIdCookieName);
+        if (previousClientId == null) {
+          if (verbose) {
+            console.log("Main: no previous cookie found for this user identifier");
+          }
+          previousClientId = Helper.UUID();
+        } else {
+          if (verbose) {
+            console.log("Main: a previous cookie has been found for this user : " + previousClientId);
+          }
         }
-        previousClientId = Helper.UUID();
-      } else {
-        if (verbose) {
-          console.log("Main: a previous cookie has been found for this user : " + previousClientId);
-        }
-      }
-      clientId = previousClientId;
-      CategoriesStorage.setUserId(clientId);
-      Helper.setCookie(clientIdCookieName, clientId, 100);
+        clientId = previousClientId;
+        CategoriesStorage.setUserId(clientId);
+        Helper.setCookie(clientIdCookieName, clientId, 100);
 
-      //load from firebase the picture the user took in previous session
-      return;
-      var allCat = CategoriesStorage.categories
-      allCat.forEach(function(cat) {
-        var catName = cat.label;
-        Firebase.getImgListForUser(clientId, catName).then(function(listIds) {
-          listIds.forEach(function(pictId) {
-            Firebase.downloadImageAsBlob(catName, pictId).then(function(wrapper) {
-              Firebase.getPointsForAPicture(pictId).then(function(points) {
-                CategoriesStorage.appendPictureWrapperToACat(wrapper.categoryName, wrapper.imageId, points, wrapper.blob);
+        //load from firebase the picture the user took in previous session
+        //return;
+        var allCat = CategoriesStorage.categories
+        allCat.forEach(function(cat) {
+          var catName = cat.label;
+          Firebase.getImgListForUser(clientId, catName).then(function(listIds) {
+            listIds.forEach(function(pictId) {
+              Firebase.downloadImageAsBlob(catName, pictId).then(function(wrapper) {
+                Firebase.getPointsForAPicture(pictId).then(function(points) {
+                  CategoriesStorage.appendPictureWrapperToACat(wrapper.categoryName, wrapper.imageId, points, wrapper.blob);
+                }).catch(function(error) {
+                  console.error("Cannot download the points for the image " + catName, " ", pictId, " ", error);
+                });
               }).catch(function(error) {
-                console.error("Cannot download the points for the image " + catName, " ", pictId, " ", error);
+                console.error("Cannot download an image for the cat " + catName, " ", pictId, " ", error);
               });
-            }).catch(function(error) {
-              console.error("Cannot download an image for the cat " + catName, " ", pictId, " ", error);
-            });
-          })
-        }).catch(function(error) {
-          console.error("Cannot get a list of image for the cat " + catName, " ", error);
+            })
+          }).catch(function(error) {
+            console.error("Cannot get a list of image for the cat " + catName, " ", error);
+          });
         });
-      });
 
-    })();
+      })();
 
 
-    var userTookPicture = function(data) {
-      var catName = CategoriesStorage.getActualCategory().label;
-      if (verbose) {
-        console.log("Manager: The user took a picture for the category \"" + catName + "\"");
+      var userTookPicture = function(data) {
+
+        if (false) {
+
+
+          return;
+        }
+
+        var catName = CategoriesStorage.getActualCategory().label;
+        if (verbose) {
+          console.log("Manager: The user took a picture for the category \"" + catName + "\"");
+        }
+        proceedImage(catName, data);
+
+        CategoriesStorage.proposeNextCategory();
+        CategoriesLayout.displayCategory(CategoriesStorage.getActualCategory());
       }
-      proceedImage(catName, data);
 
-      CategoriesStorage.proposeNextCategory();
-      CategoriesLayout.displayCategory(CategoriesStorage.getActualCategory());
-    }
+      var proceedImage = function(catName, data) {
+        //create a unique identifier for this picture
+        var imageId = Helper.UUID();
 
-    var proceedImage = function(catName, data) {
-      //create a unique identifier for this picture
-      var imageId = Helper.UUID();
-
-      //turn the blob to a base64 string to send it to the pif_paf server
-      var reader = new FileReader();
-      reader.onload = function() {
-        var json = {};
-        json.image = reader.result;
-
-        Server.requestPifPafForPoints(json).then(function(pointsText) {
+        getPointsFromPifPaf(data).then(function(points) {
           if (verbose) {
             console.log("Main: received some points from the pif paf server, time to save them in memory and send them to firebase")
           }
-          var points = JSON.parse(pointsText);
           CategoriesStorage.appendPictureWrapperToACat(catName, imageId, points, data);
           Firebase.saveImage(clientId, imageId, data, catName, new Date(), navigator.userAgent, points);
 
@@ -99,38 +99,58 @@
           Firebase.saveImage(clientId, imageId, data, catName, new Date(), navigator.userAgent, null);
         })
 
-      }
-      reader.readAsDataURL(data);
-    }
 
-
-    var systemTookPicture = function(data, drawLivePoints) {
-      if (verbose) {
-        console.log("Manager: The system took a picture");
       }
 
-      if (drawLivePoints) {
-        var reader = new FileReader();
-        reader.onload = function() {
-          var json = {};
-          json.image = reader.result;
-          PifPafBuffer.sendPictureToPifPaf(json);
+      var getPointsFromPifPaf = function(image) {
+        var promise = new Promise(function(resolve, reject) {
+
+
+            var reader = new FileReader();
+            reader.onload = function() {
+              var json = {};
+              json.image = reader.result;
+              Server.requestPifPafForPoints(json).then(function(pointsText) {
+                var points = JSON.parse(pointsText);
+                resolve(points);
+              }).catch(function(error) {
+                reject(error);
+              })
+
+            }
+            reader.readAsDataURL(image);
+          });
+
+          return promise;
         }
-        reader.readAsDataURL(data);
-      }
-    }
+
+        var systemTookPicture = function(data, drawLivePoints) {
+          if (verbose) {
+            console.log("Manager: The system took a picture");
+          }
+
+          if (drawLivePoints) {
+            var reader = new FileReader();
+            reader.onload = function() {
+              var json = {};
+              json.image = reader.result;
+              PifPafBuffer.sendPictureToPifPaf(json);
+            }
+            reader.readAsDataURL(data);
+          }
+        }
 
 
-    return {
-      userTookPicture: userTookPicture,
-      systemTookPicture: systemTookPicture,
-      clientId:clientId
-    }
+        return {
+          userTookPicture: userTookPicture,
+          systemTookPicture: systemTookPicture,
+          clientId: clientId
+        }
 
-  }();
+      }();
 
-  CameraEvents.setManager(Manager);
-  App.Manager = Manager;
-  window.App = App;
+      CameraEvents.setManager(Manager);
+      App.Manager = Manager;
+      window.App = App;
 
-})(window);
+    })(window);
